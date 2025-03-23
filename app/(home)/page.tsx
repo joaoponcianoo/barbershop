da9ae9415f9ea6e09d53af28b5f275f999ac3fb5
joaoponcianoo <joaoggponciano@gmail.com>
2025-03-23 14:55:51 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,17 @@
 import Header from "../_components/Header";
+import { format } from "date-fns/format";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div className="bg-background h-screen">
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, João!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de MMMM'", { locale: ptBR })}
+        </p>
+      </div>
     </div>
   );
 }
