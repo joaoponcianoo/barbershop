9c8b0a0149c92a9ade392c4fa1c991b79565dd0d
joaoponcianoo <joaoggponciano@gmail.com>
2025-03-23 14:41:36 -0300

    feat: add initial header component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import Header from "../_components/Header";
+
+export default function Home() {
+  return (
+    <div className="bg-background h-screen">
+      <Header />
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,13 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
-// import { Toaster } from "./_components/ui/sonner"
-// import { Footer } from "./_components/footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({ children,}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark`}>
+      <body className={inter.className}>
         {children}
-        {/* <Toaster />
-        <Footer /> */}
       </body>
     </html>
   )
