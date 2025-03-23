26baae1a68387fbef2efccfe910358a5ff019c7c
joaoponcianoo <joaoggponciano@gmail.com>
2025-03-23 18:01:21 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,12 +2,13 @@ import { format } from "date-fns/format";
 import { ptBR } from "date-fns/locale";
 import Header from "../_components/Header";
 import Search from "./_components/Search";
+import BookingItem from "../_components/BookingItem";
 
 export default function Home() {
   return (
     <div className="bg-background h-screen">
       <Header />
-      <div className="px-5 pt-5">
+      <div className="px-5 pt-6">
         <h2 className="text-xl font-bold">Olá, João!</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
@@ -15,6 +16,10 @@ export default function Home() {
       </div>
       <div className="px-5 mt-6">
         <Search />
+      </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
+        <BookingItem />
       </div>
     </div>
   );
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,16 +4,16 @@ import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 
 const Header = () => {
-  return ( 
+  return (
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
-        <Image src="/logo.png" alt="Barber Logo" height={18} width={120}/>
-        <Button variant="outline" size="icon" className="h-8 w-8">  
-          <MenuIcon size={16}/>
+        <Image src="/logo.png" alt="Barber Logo" height={18} width={120} />
+        <Button variant="outline" size="icon" className="h-8 w-8">
+          <MenuIcon size={16} />
         </Button>
       </CardContent>
     </Card>
-   );
-}
- 
+  );
+};
+
 export default Header;
